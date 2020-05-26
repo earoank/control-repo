04ab14470f0eadd390971d28907d0b068693a8a8
root <root@puppet.codespaces.io>
2020-05-26 12:11:44 +0000

    creating spec for dev

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,34 +1,9 @@
-node "node1" {
+node "node3" {
 
-notify{"*****Node1 block****":}
-
-
-include base 
-class {'::tomcat':
-
-user =>     "tomcat",
-group =>   "tomcat",
-service_state => "running",
-}
-
-tomcat::deploy{"sample":
-deploy_url => "https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war",
-
-}
-
-}
-
-node default {
-notify{"Default block":}
-include base
-}
-
-node "node2" {
-
-notify{"*****Node2 block****":}
+notify{"*****Node3 block****":}
 
 
-include base 
+#include base 
 class {'::tomcat':
 
 user =>     "tomcat",
